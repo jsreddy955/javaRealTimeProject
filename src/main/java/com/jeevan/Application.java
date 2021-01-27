b38aa136b9a69c1918d33ded4bdca169313b17de
jsreddy955 <jsjeevanreddy955@gmail.com>
2021-01-27 07:35:35 +0530

    change feature

--- src/main/java/com/jeevan/Application.java
+++ src/main/java/com/jeevan/Application.java
@@ -9,6 +9,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
                   int j=10;
+                   int m=20;//change to feateture to master
+                   int f=30;
 	}
 
 }
