10655fc4f02e4f17c76d298bac31aed1a7a495db
jsreddy955 <jsjeevanreddy955@gmail.com>
2021-01-29 06:58:41 +0530

    developementclas modiied

--- src/main/java/com/jeevan/Application.java
+++ src/main/java/com/jeevan/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+          void m1(){
+}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
                   int j=10;
                    int m=20;//change to feateture to master
                    int f=30;
+                   int k=60;
 	}
 
 }
